f5cddf0fd5008cbdbdd26a7db69c102be7887573
Joeybae <rhkdgks48@gmail.com>
2019-10-12 15:02:23 +0900

    first commit

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ function templateHTML(title, list, body){
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
+    <a href = "/create">create</a>
     ${body}
   </body>
   </html>
@@ -55,6 +56,27 @@ var app = http.createServer(function(request,response){
           });
         });
       }
+    } else if(pathname === '/create'){
+        fs.readdir('./data', function(error, filelist){
+          var title = 'WEB - create';
+          var list = templateList(filelist);
+          var template = templateHTML(title, list, `
+            <form action="http://localhost:3000/process_create"
+            method="POST">
+                <p>
+                    <input type="text" name="title" placeholder="title">
+                </p>
+                <p>
+                    <textarea name="description" placeholder="description"></textarea>
+                </p>
+                <p>
+                    <input type="submit">
+                </p>
+            </form>
+          `);
+          response.writeHead(200);
+          response.end(template);
+        });
     } else {
       response.writeHead(404);
       response.end('Not found');
