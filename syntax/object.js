64e872fa9eafeefc9e20adcb4fd17e7b07b1d56a
Joeybae <rhkdgks48@gmail.com>
2019-10-15 15:20:00 +0900

    first commit

---
+++ syntax/object.js
@@ -0,0 +1,21 @@
+//arrays
+var members = ['engoing', 'k8805', 'hoya'];
+console.log(members[1]); // k8805
+var i = 0;
+while(i < members.length){
+    console.log('array loop', members[i]);
+    i = i + 1;
+}
+
+//object
+var roles = {
+    'programmer' : 'egoing',
+    'designer' : 'k8805',
+    'manager' : 'hoya'
+}
+console.log(roles.designer);
+console.log(roles['designer']); // k8805
+
+for(var name in roles){
+    console.log('object => ', name, ', value => ', roles[name]);
+}
---
+++ syntax/object2.js
@@ -0,0 +1,13 @@
+// array, object
+var f = function(){
+    console.log(1+1);
+    console.log(1+2);
+}
+
+var a = [f];
+a[0]();
+
+var o = {
+    func:f
+}
+o.func();
